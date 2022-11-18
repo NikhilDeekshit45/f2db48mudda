c83176bc94a683de5528ca0e3321ce3993aa37a5
NikhilDeekshit45 <nikhildeekshit2@gmail.com>
2022-11-18 00:31:48 -0600

    updated

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ var mongoose = require('mongoose')
 var mongodb = require('mongodb')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  } 
+));
 var stone = require("./models/stone");
 require('dotenv').config();
 const connectionString =
@@ -38,8 +54,37 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+
+  secret: 'keyboard cat',
+
+  resave: false,
+
+  saveUninitialized: false
+
+}));
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+// passport config
+
+// Use the existing connection
+
+// The Account model  
+
+var Account =require('./models/account');
+
+ 
+
+passport.use(new LocalStrategy(Account.authenticate()));
+
+passport.serializeUser(Account.serializeUser());
+
+passport.deserializeUser(Account.deserializeUser());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/stone', stoneRouter);
