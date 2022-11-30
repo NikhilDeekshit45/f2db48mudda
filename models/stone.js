5c6559e658536bf1c4d24c89dd9eaf26d54ef0a2
NikhilDeekshit45 <nikhildeekshit2@gmail.com>
2022-11-30 16:10:59 -0600

    updated

--- models/stone.js
+++ models/stone.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose") 
 const stoneSchema = mongoose.Schema({ 
- stone_Name: String, 
- stone_Company: String, 
- stone_size: Number,
- stone_Rating: Number
+ stone_Name: {type: String,required: [true, 'Name of the stone cannot be empty']}, 
+ stone_Company: {type: String,required: [true, 'Company of the stone cannot be empty']}, 
+ stone_size: {type: Number,required: [true, 'size of the stone cannot be empty']},
+ stone_Rating: {type: Number,required: [true, 'Rating of the stone cannot be empty']}
 })
 
  
--- routes/stone.js
+++ routes/stone.js
@@ -2,6 +2,14 @@ var express = require('express');
 const stone_controllers= require('../controllers/stone');
 var router = express.Router();
 
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
+
 /* GET stones */
 router.get('/', stone_controllers.stone_view_all_Page );
 
@@ -9,10 +17,10 @@ router.get('/', stone_controllers.stone_view_all_Page );
 router.get('/detail', stone_controllers.stone_view_one_Page);
 
 /* GET create stone page */
-router.get('/create', stone_controllers.stone_create_Page);
+router.get('/create',secured, stone_controllers.stone_create_Page);
 
 /* GET create update page */
-router.get('/update', stone_controllers.stone_update_Page);
+router.get('/update',secured, stone_controllers.stone_update_Page);
 
 /* GET delete stone page */
 router.get('/delete', stone_controllers.stone_delete_Page);
